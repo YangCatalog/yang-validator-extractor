27b4d2e05cc9a55ea048657d473977a032c00af5
Carl Moberg <callemoberg@gmail.com>
2018-01-27 14:08:33 +0100

    Adjust the YANG import dir for the sync-script

--- bottle-yang-extractor-validator/main.py
+++ bottle-yang-extractor-validator/main.py
@@ -22,7 +22,7 @@ __license__ = "New-style BSD"
 __email__ = "camoberg@cisco.com"
 __version__ = "0.3"
 
-yang_import_dir = '/var/tmp/yangmodules'
+yang_import_dir = '/var/tmp/yangmodules/extracted'
 pyang_cmd = '/usr/local/bin/pyang'
 yanglint_cmd = '/usr/local/bin/yanglint'
 yanglint_version = check_output(yanglint_cmd + " --version", shell=True)
