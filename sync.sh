f672afb765f8c091d02c0692dabd77232612ad26
Carl Moberg <callemoberg@gmail.com>
2018-01-27 13:42:12 +0100

    Added small script to fetch and extract YANG Modules from IETF datatracker

--- sync.sh
+++ sync.sh
@@ -1,14 +1,25 @@
-TMPDIR=$(mktemp -d /tmp/ietfmirror.XXXXXX)
+#
+# This script fetches all published IETF RFCs and drafts, extracts any
+# found YANG modules and packs it up in a tarball.
+#
+
+TMPDIR=$(mktemp -d /var/tmp/ietfmirror)
+if [ $? -ne 0 ]; then
+	echo "$0: Can't create temp file, exiting..."
+	exit 1
+fi
+
 echo "Fetching files into $TMPDIR"
 
-rsync -avz --no-l --include="draft*.txt" --exclude="*" --delete rsync.ietf.org::internet-drafts $TMPDIR/my-id-mirror
-rsync -avz --no-l --include="rfc*.txt"   --exclude="*" --delete rsync.ietf.org::rfc $TMPDIR/my-rfc-mirror
+rsync -az --no-l --include="draft*.txt" --exclude="*" --delete rsync.ietf.org::internet-drafts $TMPDIR/my-id-mirror
+rsync -az --no-l --include="rfc*.txt"   --exclude="*" --delete rsync.ietf.org::rfc $TMPDIR/my-rfc-mirror
 
 mkdir $TMPDIR/all-yang
 
 for file in $(egrep -l '^[ \t]*(sub)?module +[\\'\"]?[-A-Za-z0-9]*[\\'\"]? *\{.*$' $TMPDIR/my-id-mirror/*)  ; do xym --dstdir $TMPDIR/all-yang --strict True $file; done
 for file in $(egrep -l '^[ \t]*(sub)?module +[\\'\"]?[-A-Za-z0-9]*[\\'\"]? *\{.*$' $TMPDIR/my-rfc-mirror/*) ; do xym --dstdir $TMPDIR/all-yang --strict True $file; done
 
+# Should probably exclude more weird non-useful modules here
 tar --exclude="example*" -zcvf ./all-yang.tgz -C $TMPDIR all-yang
 
-rm -rf $TMPDIR
+rm -rf mkdir $TMPDIR/all-yang
