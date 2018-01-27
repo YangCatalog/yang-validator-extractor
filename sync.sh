736e6cc0d1f2f1888cf0bb3bd077a873bca7832c
root <root@ip-172-31-28-199.us-west-2.compute.internal>
2018-01-27 13:07:01 +0000

    Stop packing up things, just dump the modules in a known directory

--- sync.sh
+++ sync.sh
@@ -2,10 +2,13 @@
 # This script fetches all published IETF RFCs and drafts, extracts any
 # found YANG modules and packs it up in a tarball.
 #
+TMPDIR=/var/tmp/yangmodules/work
+EXTRACTDIR=/var/tmp/yangmodules/extracted
+mkdir -pv $TMPDIR
+mkdir -pv $EXTRACTDIR
 
-TMPDIR=$(mktemp -d /var/tmp/yangmodules)
 if [ $? -ne 0 ]; then
-	echo "$0: Can't create temp file, exiting..."
+	echo "$0: Can't create temp directory, exiting..."
 	exit 1
 fi
 
@@ -14,12 +17,6 @@ echo "Fetching files into $TMPDIR"
 rsync -az --no-l --include="draft*.txt" --exclude="*" --delete rsync.ietf.org::internet-drafts $TMPDIR/my-id-mirror
 rsync -az --no-l --include="rfc*.txt"   --exclude="*" --delete rsync.ietf.org::rfc $TMPDIR/my-rfc-mirror
 
-mkdir $TMPDIR/all-yang
+for file in $(egrep -l '^[ \t]*(sub)?module +[\\'\"]?[-A-Za-z0-9]*[\\'\"]? *\{.*$' $TMPDIR/my-id-mirror/*)  ; do xym --dstdir $EXTRACTDIR --strict True $file; done
+for file in $(egrep -l '^[ \t]*(sub)?module +[\\'\"]?[-A-Za-z0-9]*[\\'\"]? *\{.*$' $TMPDIR/my-rfc-mirror/*) ; do xym --dstdir $EXTRACTDIR --strict True $file; done
 
-for file in $(egrep -l '^[ \t]*(sub)?module +[\\'\"]?[-A-Za-z0-9]*[\\'\"]? *\{.*$' $TMPDIR/my-id-mirror/*)  ; do xym --dstdir $TMPDIR/all-yang --strict True $file; done
-for file in $(egrep -l '^[ \t]*(sub)?module +[\\'\"]?[-A-Za-z0-9]*[\\'\"]? *\{.*$' $TMPDIR/my-rfc-mirror/*) ; do xym --dstdir $TMPDIR/all-yang --strict True $file; done
-
-# Should probably exclude more weird non-useful modules here
-tar --exclude="example*" -zcvf ./all-yang.tgz -C $TMPDIR all-yang
-
-rm -rf mkdir $TMPDIR/all-yang
