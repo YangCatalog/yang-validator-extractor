3d41efa8638d3f12229f192807a7eaf11a9f8d88
miroslav.kovac <miroslav.kovac@pantheon.tech>
2019-07-09 16:18:13 +0200

    Update copyrights
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
+# Copyright The IETF Trust 2019, All Rights Reserved
+# Copyright 2015 Cisco and its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = "Miroslav Kovac, Carl Moberg"
+__copyright__ = "Copyright 2015 Cisco and its affiliates, Copyright The IETF Trust 2019, All Rights Reserved"
+__license__ = "Apache License, Version 2.0"
+__email__ = "miroslav.kovac@pantheon.tech, camoberg@cisco.com"
+
 from setuptools import setup
 import os
 
@@ -13,8 +33,8 @@ setup(
         'A web application that allows you to fetch, extract and validate YANG modules by RFC number, by IETF draft name, or by uploading YANG files.'),
     long_description=read('README.md'),
     packages=['bottle-yang-extractor-validator'],
-    author='Carl Moberg',
-    author_email='camoberg@cisco.com',
+    author='Carl Moberg, Miroslav Kovac',
+    author_email='camoberg@cisco.com, miroslav.kovac@pantheon.tech',
     license='New-style BSD',
     url='https://github.com/cmoberg/bottle-yang-extractor-validator',
     install_requires=['xym>=0.4.4', 'pyang==1.7.8', 'Django>=2.1.2', 'uWSGI>=2.0.18', 'certifi==2018.11.29',
--- yangvalidator/yangvalidator/views.py
+++ yangvalidator/yangvalidator/views.py
@@ -1,3 +1,18 @@
+# Copyright The IETF Trust 2019, All Rights Reserved
+# Copyright 2015 Cisco and its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import cgi
 import json
 import logging
@@ -17,7 +32,7 @@ from django.shortcuts import render
 from xym import xym
 
 __author__ = "Miroslav Kovac, Carl Moberg"
-__copyright__ = "Copyright 2019 Cisco and its affiliates"
+__copyright__ = "Copyright 2015 Cisco and its affiliates, Copyright The IETF Trust 2019, All Rights Reserved"
 __license__ = "Apache License, Version 2.0"
 __email__ = "miroslav.kovac@pantheon.tech, camoberg@cisco.com"
 __version__ = "0.4.0"
