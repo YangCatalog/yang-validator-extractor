b7c97d9fbe2f7810cc3006cdbbfea486d5c89d09
Slavomir Mazur <slavomir.mazur@pantheon.tech>
2022-08-09 12:27:07 +0200

    chore: update title while tracking API requests using Matomo
    - API yangvalidator added before tracked API request URL
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- yangvalidator/middleware.py
+++ yangvalidator/middleware.py
@@ -1,25 +1,10 @@
 from urllib.error import URLError
-from urllib.parse import urlencode
 
 import ipware
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
 from piwikapi.tests.request import FakeRequest
 from piwikapi.tracking import PiwikTracker
-
-
-class ExtendedPiwikTracker(PiwikTracker):
-    """ We have to extend this to add the set_user_id """
-    user_id = None
-
-    def set_user_id(self, user_id: str):
-        self.user_id = user_id
-
-    def _get_request(self, id_site: int):
-        url = super()._get_request(id_site)
-        if self.user_id:
-            url += '&{}'.format(urlencode({'uid': self.user_id}))
-        return url
 
 
 class MatomoMiddleware:
@@ -32,7 +17,7 @@ class MatomoMiddleware:
         if should_skip(request):
             return response
 
-        site_id = getattr(settings, 'MATOMO_SITE_ID', 1)
+        site_id = getattr(settings, 'MATOMO_SITE_ID')
         if not site_id:
             return response
 
@@ -51,34 +36,29 @@ class MatomoMiddleware:
         for key in keys_to_serialize:
             if key in request.META:
                 data[key] = request.META[key]
-        user_id = ''
-        if getattr(settings, 'MATOMO_TRACK_USER_ID', None) and request.user and request.user.pk:
-            user_id = request.user.pk
         client_ip = ipware.get_client_ip(request)
         try:
-            record_analytic(data, client_ip, user_id)
+            record_analytic(data, client_ip)
         except URLError:
             pass
 
         return response
 
 
-def record_analytic(headers: dict, client_ip: str, user_id: str) -> None:
+def record_analytic(headers: dict, client_ip: str) -> None:
     """ Send analytics data to Piwik/Matomo """
     # Use "FakeRequest" because we had to serialize the real request
     fake_request = FakeRequest(headers)
 
-    piwik_tracker = ExtendedPiwikTracker(settings.MATOMO_SITE_ID, fake_request)
+    piwik_tracker = PiwikTracker(settings.MATOMO_SITE_ID, fake_request)
     piwik_tracker.set_api_url(settings.MATOMO_API_URL)
     if settings.MATOMO_TOKEN_AUTH:
         piwik_tracker.set_token_auth(settings.MATOMO_TOKEN_AUTH)
         piwik_tracker.set_ip(client_ip)
-    if getattr(settings, 'MATOMO_TRACK_USER_ID', None) and user_id:
-        piwik_tracker.set_user_id(user_id)
     visited_url = fake_request.META['PATH_INFO'][:1000]
-    piwik_tracker.do_track_page_view(visited_url)
+    piwik_tracker.do_track_page_view('API yangvalidator {}'.format(visited_url))
 
 
 def should_skip(request: WSGIRequest) -> bool:
     """ Check whether the request is not just a ping. """
-    return 'yangvalidator/v2/' not in request.path or 'yangvalidator/v2/ping' in request.path
+    return 'yangvalidator/v2/' not in request.path or 'v2/ping' in request.path or 'v2/versions' in request.path
