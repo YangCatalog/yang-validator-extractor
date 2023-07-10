58f4f2eb6406839183eaa1d59985dcb33e58b4ce
richardzilincikPantheon <85497288+richardzilincikPantheon@users.noreply.github.com>
2023-07-10 17:57:16 +0200

    chore: filter healthchecks out of gunicorn logs (#140)

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,5 @@
+import logging
+
 bind = 'unix:/var/run/yang/yangvalidator.sock'
 # umask = os.umask('007')
 
@@ -19,3 +21,13 @@ loglevel = 'debug'
 # change log format
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 worker_class = 'gevent'
+
+
+class HealthcheckFilter(logging.Filter):
+    def filter(self, record):
+        return record.getMessage().find('Amazon-Route53-Health-Check-Service') == -1
+
+
+def on_starting(server):
+    server.log.access_log.addFilter(HealthcheckFilter())
+    server.log.error_log.addFilter(HealthcheckFilter())
